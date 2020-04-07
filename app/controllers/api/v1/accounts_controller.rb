f682d4d2476da5f763159afe631221d1c50dc337
Marcelo Escobar <escobar@br.systems>
2020-04-07 15:05:45 -0300

    fix api messages

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -3,23 +3,29 @@ class Api::V1::AccountsController < ApplicationController
   before_action :authorize, only: [:show, :transfer]
 
   def show
-    account_id = balance_params[:id]
+    account_id = balance_params[:account_id]
     account = Account.where(account_id: account_id).first
-    render json: account.nil? ? { error: 'Informar um ID de conta válido para consulta.' } : { account: account.balance }
+
+    if account.nil?
+      api_message('Informar um ID de conta válido para consulta.')
+    else
+     api_message(account.balance, type = :account)
+    end
   end
 
   def create
     acc_params = account_params
-    custom_id = acc_params[:id]
-    account_name = acc_params[:name]
-    account_balance = acc_params[:balance].to_i
+    custom_id = acc_params[:account_id]
+    account_name = acc_params[:account_name]
+    account_balance = acc_params[:initial_amount].to_i
+
+    return api_message('Informe o nome do Cliente.') if account_name.nil?
 
     if !custom_id.nil?
-      custom_id = acc_params[:id]
-      return render json: { error: 'ID deve ser um número.' } if custom_id == 0
+      return api_message('ID deve ser um número.') if custom_id == 0
 
       account = Account.where(account_id: custom_id).first
-      return render json: { customer: 'ID já está em uso, por favor escolha outro.' } unless account.nil?
+      return api_message('ID já está em uso, por favor escolha outro.')  unless account.nil?
     end
 
     create_account(account_name, account_balance, custom_id)
@@ -29,21 +35,19 @@ class Api::V1::AccountsController < ApplicationController
     transfer = transfer_params
     @amount = transfer[:amount].to_i
 
-    return render json: { error: 'Informar valor a ser transferido.' } if @amount <= 0
+    return api_message('Informar valor a ser transferido.') if @amount <= 0
 
-    @origin_account = Account.where(account_id: transfer[:source]).first
-    return render json: { error: 'Conta de origem inválida.' } if @origin_account.nil?
+    @origin_account = Account.where(account_id: transfer[:source_account_id]).first
+    return api_message('Conta de origem inválida.') if @origin_account.nil?
 
-    @target_account = Account.where(account_id: transfer[:destination]).first
-    return render json: { error: 'Conta de destino inválida.' } if @target_account.nil?
+    @target_account = Account.where(account_id: transfer[:destination_account_id]).first
+    return api_message('Conta de destino inválida.')  if @target_account.nil?
 
-    return render json: { error: 'Saldo insuficiente.' } if @origin_account.balance < @amount
-
+    return api_message('Saldo insuficiente.') if @origin_account.balance < @amount
     execute_transfer
 
-    render json: { message: 'Transação efetuada.' }
+    api_message('Transação efetuada.', type = :message)
   end
-
 
   private
   def execute_transfer
@@ -56,10 +60,8 @@ class Api::V1::AccountsController < ApplicationController
     end
   end
 
-  def api_message(msg, type = :msg)
-    render json: { type: msg }
-    # render json: { customer: 'ID já está em uso, por favor escolha outro.' }
-    # exit
+  def api_message(msg, type = :error)
+    render json: { type => msg }
   end
 
   def create_account(account_name, account_balance, account_id = nil)
@@ -67,19 +69,19 @@ class Api::V1::AccountsController < ApplicationController
     customer.name = account_name
 
     unless customer.save!
-      return render json: { error: 'erro ao criar cliente' }
+      api_message('erro ao criar cliente')
     end
 
     @account = Account.new
-    @account.account_id = account_id.to_i.to_s unless account_id.nil?
+    @account.account_id = account_id unless account_id.nil? #.to_i.to_s
     @account.customer_id = customer.id
     @account.balance = account_balance
 
     if @account.save!
       set_account_id if account_id.nil?
-      return render json: { id: @account.account_id, token: customer.access_token }
+      return render json: { account_id: @account.account_id, token: customer.access_token }
     else
-      return render json: { error: 'erro ao criar conta' }
+      return api_message('erro ao criar conta')
     end
   end
 
@@ -89,14 +91,14 @@ class Api::V1::AccountsController < ApplicationController
   end
 
   def account_params
-    params.permit(:id, :name, :balance)
+    params.permit(:account_id, :account_name, :initial_amount)
   end
 
   def balance_params
-    params.permit(:id)
+    params.permit(:account_id)
   end
 
   def transfer_params
-    params.permit(:source, :destination, :amount)
+    params.permit(:source_account_id, :destination_account_id, :amount)
   end
 end
